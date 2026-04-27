acef31a7608ab61945f7ece6d9d8431720a784cb
ShubhamJoshi-dev <codefortanke@gmail.com>
2026-04-27 21:48:06 +0545

    integrate typeorm with the pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import configuration from './config/configuration';
+import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { HealthModule } from './modules/health/health.module';
@@ -16,17 +16,7 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
       load: [configuration],
       envFilePath: '.env',
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        url: configService.get<string>('database.url'),
-        autoLoadEntities: true,
-        synchronize: configService.get<string>('nodeEnv') !== 'production',
-        logging: configService.get<string>('nodeEnv') === 'development',
-      }),
-      inject: [ConfigService],
-    }),
+    DatabaseModule,
     TokenBlocklistModule,
     UsersModule,
     AuthModule,
